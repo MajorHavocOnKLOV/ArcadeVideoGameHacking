a7222eff2048d385ccc699890710421c9c9a5cc7
Franklin Bowen <franklin@franklinbowen.net>
2023-12-26 08:22:16 -0500

    Fix generated files so they correctly reverse sort on count (1st column) and then sort on address (2nd column). Previously, it was just sorting numeric reverse which works until you get rows where the counts are same https://unix.stackexchange.com/questions/52762/trying-to-sort-on-two-fields-second-then-first

--- Super_Punchout/generate_files.sh
+++ Super_Punchout/generate_files.sh
@@ -1,40 +1,40 @@
 # Count the number of times a subroutine is called
 echo "# In *nix (or WSL with *nix), type:
 cd /mnt/e/git-repos/ArcadeVideoGameHacking.git/Super_Punchout
-cat spnchout.asm | grep CALL | cut -c28- | sort | uniq -c | sort -nr >> calls_counted.txt
+cat spnchout.asm | grep CALL | cut -c28- | sort | uniq -c | sort -k1,1r -k2,2 >> calls_counted.txt
 
 " > calls_counted.txt
-cat spnchout.asm | grep CALL | cut -c28- | sort | uniq -c | sort -nr >> calls_counted.txt
+cat spnchout.asm | grep CALL | cut -c28- | sort | uniq -c | sort -k1,1r -k2,2 >> calls_counted.txt
 
 
 
 # RAM access C000-C3FF.txt
 echo "# In *nix (or WSL with *nix), type:
 cd /mnt/e/git-repos/ArcadeVideoGameHacking.git/Super_Punchout
-cat spnchout.asm | grep '\$C[0-3][0-9A-F][0-9A-F]' | cut -c28- | grep -v '^SP' | sed 's/.*\(C[0-3][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -nr >> C000-C3FF.txt
+cat spnchout.asm | grep '\$C[0-3][0-9A-F][0-9A-F]' | cut -c28- | grep -v '^SP' | sed 's/.*\(C[0-3][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -k1,1r -k2,2 >> C000-C3FF.txt
 
 " > C000-C3FF.txt
-cat spnchout.asm | grep '\$C[0-3][0-9A-F][0-9A-F]' | cut -c28- | grep -v '^SP' | sed 's/.*\(C[0-3][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -nr >> C000-C3FF.txt
+cat spnchout.asm | grep '\$C[0-3][0-9A-F][0-9A-F]' | cut -c28- | grep -v '^SP' | sed 's/.*\(C[0-3][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -k1,1r -k2,2 >> C000-C3FF.txt
 
 
 
 # RAM access C400-CFFF.txt
 echo "# In *nix (or WSL with *nix), type:
 cd /mnt/e/git-repos/ArcadeVideoGameHacking.git/Super_Punchout
-cat spnchout.asm | grep '\$C[4-9A-F][0-9A-F][0-9A-F]' | cut -c28- | grep -v '^SP' | sed 's/.*\(C[0-3][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -nr >> C400-CFFF.txt
+cat spnchout.asm | grep '\$C[4-9A-F][0-9A-F][0-9A-F]' | cut -c28- | grep -v '^SP' | sed 's/.*\(C[0-3][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -k1,1r -k2,2 >> C400-CFFF.txt
 
 " > C400-CFFF.txt
-cat spnchout.asm | grep '\$C[4-9A-F][0-9A-F][0-9A-F]' | cut -c28- | grep -v '^SP' | sed 's/.*\(C[0-3][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -nr >> C400-CFFF.txt
+cat spnchout.asm | grep '\$C[4-9A-F][0-9A-F][0-9A-F]' | cut -c28- | grep -v '^SP' | sed 's/.*\(C[0-3][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -k1,1r -k2,2 >> C400-CFFF.txt
 
 
 
 # RAM access D000-FFFF.txt
 echo "# In *nix (or WSL with *nix), type:
 cd /mnt/e/git-repos/ArcadeVideoGameHacking.git/Super_Punchout
-cat spnchout.asm | grep '\$[DEF][0-9A-F][0-9A-F][0-9A-F]' | cut -c28- | sed 's/.*\([DEF][[:alnum:]][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -nr >> D000-FFFF.txt
+cat spnchout.asm | grep '\$[DEF][0-9A-F][0-9A-F][0-9A-F]' | cut -c28- | sed 's/.*\([DEF][[:alnum:]][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -k1,1r -k2,2 >> D000-FFFF.txt
 
 " > D000-FFFF.txt
-cat spnchout.asm | grep '\$[DEF][0-9A-F][0-9A-F][0-9A-F]' | cut -c28- | sed 's/.*\([DEF][[:alnum:]][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -nr >> D000-FFFF.txt
+cat spnchout.asm | grep '\$[DEF][0-9A-F][0-9A-F][0-9A-F]' | cut -c28- | sed 's/.*\([DEF][[:alnum:]][[:alnum:]][[:alnum:]]\).*/\1/' | sort | uniq -c | sort -k1,1r -k2,2 >> D000-FFFF.txt
 
 
 
